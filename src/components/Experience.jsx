4eecdb2b44278a5952fb10880ea06ca2de565f0a
rashidsharif <rashidsharif213@gmail.com>
2023-07-04 17:32:22 +0500

    first commit

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -3,18 +3,20 @@ import Feature from "./Feature";
 
 function Experience() {
   return (
-    <div className="experience flex flex-col items-center justify-start px-[5rem] bg-[#020917] h-[60rem] pt-[18rem] mt-[-10rem] relative z-[2] rounded-b-[5rem]">
-      {/* titld icon */}
+    <div className="experience flex flex-col items-center justify-start px-[5rem] bg-[#020917] pt-[18rem] mt-[-10rem] relative z-[2] rounded-b-[5rem]">
+      {/* title icon */}
       <img src={require("../img/Path 318.png")} alt="" className="w-[5rem]" />
       {/* heading */}
-      <div className="headline mt-7 flex flex-col items-center text-[2rem]">
-        <span>An Amazing App Can Change Your Daily Life</span>
-        <span>
-          <b>Music Experience</b>
-        </span>
+      <div className="headline mt-7 flex flex-col items-center">
+        <h2 className="text-2xl sm:text-3xl text-center md:text-4xl">
+          An Amazing App Can Change Your Daily Life
+        </h2>
+        <h3 className="text-lg sm:text-xl md:text-2xl font-bold">
+          Music Experience
+        </h3>
       </div>
       {/* features  */}
-      <div className="feature flex items-center justify-around mt-[6rem] w-[100%]">
+      <div className="feature flex flex-wrap items-center justify-around mt-[6rem] w-[100%] max-w-[80rem]">
         <Feature icon="Group 2" title="For Live Music" />
         <Feature icon="music icon" title="For Daily Music" />
         <Feature icon="Group 4" title="For Artists" />
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,22 +4,25 @@ import CenterMenu from "./CenterMenu";
 function Header() {
   const buttonStyle =
     "border-[2px] rounded-[10px] border-[#232A4E] px-[25px] py-[7px]";
+
   return (
-    <div className="header bg-[#081730] flex items-center justify-between px-[5rem] pt-[2.4rem] text-[0.8rem]">
+    <div className="header bg-[#081730] flex flex-wrap items-center justify-between px-[5rem] pt-[2.4rem] text-[0.8rem]">
       {/* logo */}
-      <img
-        src={require("../img/MuzicLogo.png")}
-        alt=""
-        className="logo  w-[42px] h-[42px]"
-      />
+      <div className="logo-container mb-[1rem] sm:mb-0">
+        <img
+          src={require("../img/MuzicLogo.png")}
+          alt=""
+          className="logo w-[42px] h-[42px]"
+        />
+      </div>
       {/* side menu */}
       <CenterMenu />
       {/* buttons */}
-      <div className="buttons flex">
-        <button className={`mr-[35px] hover:bg-[#232A4E] ` + buttonStyle}>
+      <div className="buttons flex flex-wrap sm:flex-nowrap">
+        <button className={`mr-[35px] mb-[1rem] sm:mb-0 hover:bg-[#232A4E] ` + buttonStyle}>
           Log in
         </button>
-        <button className={buttonStyle+` bg-[#232A4E]`}>Sign up</button>
+        <button className={buttonStyle + ` bg-[#232A4E]`}>Sign up</button>
       </div>
     </div>
   );
